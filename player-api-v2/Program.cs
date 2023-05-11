e5cc7839fd8f82e1eb3b417998f9f2119c3d501c
Crystal Studio <notwirachai@gmail.com>
2023-05-11 19:42:21 +0700

    fix(player): set header

--- player-api-v2/Program.cs
+++ player-api-v2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient();
@@ -27,15 +29,9 @@ if (app.Environment.IsDevelopment())
 
 // กำหนดข้อมูลที่จำเป็นในการเรียกใช้ API
 string titleId = "2506C";
-//string playFabId = "FA52F517CD2A0D7D";
-string apiKey = "NHw5dmRUeXcrK1ljYXZSYmhETlMzMGd1U0U3OHozRVhUTjgycFA5bitRZnA0PXx7ImkiOiIyMDIzLTA1LTExVDA3OjE2OjQxWiIsImlkcCI6IkN1c3RvbSIsImUiOiIyMDIzLTA1LTEyVDA3OjE2OjQxWiIsImZpIjoiMjAyMy0wNS0xMVQwNzoxNjo0MVoiLCJ0aWQiOiJKaExrbDlrNE11WSIsImlkaSI6Ik5vVFRZODg4IiwiaCI6IjI1NENFM0RDRjEyRTJCMyIsImVjIjoidGl0bGVfcGxheWVyX2FjY291bnQhNkMwREQzOEI2MzlDRURDQy8yNTA2Qy9GNkExN0YzQjI4QTk4RUI0LzYzRkZBNEMwQjNCNUI5Q0MvIiwiZWkiOiI2M0ZGQTRDMEIzQjVCOUNDIiwiZXQiOiJ0aXRsZV9wbGF5ZXJfYWNjb3VudCJ9";
-
 app.UseHttpsRedirection();
 
-
-Console.WriteLine("titleId =>> "+ "2506C"+ titleId);
-
-app.MapGet("/player", async (HttpClient httpClient) =>
+app.MapGet("/player", async (HttpClient httpClient, [FromHeader(Name = "X-EntityToken")] string entityToken) =>
 {
     // กำหนด URL ของ API
     string url = $"https://{titleId}.playfabapi.com/Inventory/GetInventoryCollectionIds";
@@ -51,7 +47,7 @@ app.MapGet("/player", async (HttpClient httpClient) =>
     var content = new StringContent(json, Encoding.UTF8, "application/json");
 
     // กำหนด header ของ HTTP request
-    httpClient.DefaultRequestHeaders.Add("X-EntityToken", apiKey);
+    httpClient.DefaultRequestHeaders.Add("X-EntityToken", entityToken);
 
     // ส่ง HTTP POST request ไปยัง API ผ่าน HttpClient
     HttpResponseMessage response = await httpClient.PostAsync(url, content);
@@ -59,7 +55,7 @@ app.MapGet("/player", async (HttpClient httpClient) =>
     string responseBody = await response.Content.ReadAsStringAsync();
     dynamic jsonResult = JsonSerializer.Deserialize<dynamic>(responseBody);
     Console.WriteLine("response =>> " + responseBody);
-
+    Console.WriteLine("jsonResult =>> " + jsonResult);
     // ส่ง JSON response กลับไปหา client
     return Results.Json(jsonResult);
 })
