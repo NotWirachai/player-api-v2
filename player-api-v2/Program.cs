39d2acde4ad03d8597c9a3981b072b339fa3f810
Crystal Studio <notwirachai@gmail.com>
2023-05-15 15:44:15 +0700

    fix(player): create collectionId

--- player-api-v2/Program.cs
+++ player-api-v2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -33,30 +34,78 @@ app.UseHttpsRedirection();
 
 app.MapGet("/player", async (HttpClient httpClient, [FromHeader(Name = "X-EntityToken")] string entityToken) =>
 {
-    // กำหนด URL ของ API
     string url = $"https://{titleId}.playfabapi.com/Inventory/GetInventoryCollectionIds";
 
-    // กำหนดข้อมูลใน request body
     var data = new Dictionary<string, object>
     {
         { "Count", 1 },
     };
 
-    // แปลงข้อมูลใน request body เป็น JSON format
     string json = JsonSerializer.Serialize(data);
     var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-    // กำหนด header ของ HTTP request
     httpClient.DefaultRequestHeaders.Add("X-EntityToken", entityToken);
 
-    // ส่ง HTTP POST request ไปยัง API ผ่าน HttpClient
     HttpResponseMessage response = await httpClient.PostAsync(url, content);
-    // ดึงข้อมูลใน response body และแปลงเป็น JSON object
     string responseBody = await response.Content.ReadAsStringAsync();
     dynamic jsonResult = JsonSerializer.Deserialize<dynamic>(responseBody);
-    
-    // ส่ง JSON response กลับไปหา client
-    return Results.Json(jsonResult);
+
+    if (jsonResult.ValueKind == JsonValueKind.Object)
+    {
+        if (jsonResult.TryGetProperty("data", out JsonElement dataElement) && dataElement.ValueKind == JsonValueKind.Object)
+        {
+            if (dataElement.TryGetProperty("CollectionIds", out JsonElement collectionIdsElement) && collectionIdsElement.ValueKind == JsonValueKind.Array && collectionIdsElement.GetArrayLength() > 0)
+            {
+                // CollectionIds has values
+                Console.WriteLine("CollectionIds has values");
+                // return new JsonResult(collectionIdsElement);
+                return Results.Json(jsonResult);
+            }
+            else
+            {
+                // CollectionIds is empty
+                var requestBody = new Dictionary<string, object>
+                    {
+                        { "Amount", 1 },
+                        { "Entity", new Dictionary<string, object>
+                            {
+                                { "Id", "5C22B048B1D16E86" },
+                                { "Type", "title_player_account" },
+                                { "TypeString", "title_player_account" }
+                            }
+                        },
+                        { "Item", new Dictionary<string, object>
+                            {
+                                { "Id", "e9086e27-c51c-4072-ba27-621698b81fe1" }
+                            }
+                        },
+                        {"CollectionId","State1-0" }
+                    };
+
+                string collectionJson = JsonSerializer.Serialize(requestBody);
+                var collectionContent = new StringContent(collectionJson, Encoding.UTF8, "application/json");
+
+                string urlCollectionIds = $"https://{titleId}.playfabapi.com/Inventory/AddInventoryItems";
+                HttpResponseMessage responseCollectionIds = await httpClient.PostAsync(urlCollectionIds, collectionContent);
+                string responseBodyCollectionIds = await responseCollectionIds.Content.ReadAsStringAsync();
+                dynamic jsonResultCollectionIds = JsonSerializer.Deserialize<dynamic>(responseBody);
+                return Results.Json(jsonResultCollectionIds);
+               // return new JsonResult("CollectionIds is empty");
+            }
+        }
+        else
+        {
+            // CollectionIds is missing
+            return new JsonResult("CollectionIds is missing");
+
+        }
+    }
+    else
+    {
+        // Invalid response format
+        return new JsonResult("Invalid response format");
+    }
+
 })
 .WithName("Player")
 .WithOpenApi();
